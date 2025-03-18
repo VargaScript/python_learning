1e4d554634b3a789f9bc1be90a1c0eb4be321cb7
VargaScript <wolfolkvargas295478@gmail.com>
2025-03-18 14:33:02 -0700

    First python project, number guessing game

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # python_learning
-Every project on my Python learning path
+Every project on my Python learning path, this learning path is with the intention to become a Machine Learning/Deep Learning Engineer. I'm interested also on Data Science and Data Engineering, but mostly focused on both Machine Learning/Deep Learning Engineer.
+
+
+First project: Number Guessing Game
---
+++ number_guessing_game/number_guessing_game.py
@@ -0,0 +1,41 @@
+#Input a number and try to find the match
+
+"""
+Necessary steps
+    1. The user gives a range to generate a random number to guess on
+    2. Store the user´s number
+    3. Compare the number and print if the user´s above or below the number
+"""
+
+import random
+
+ranges = input("Enter 2 numbers for the range of numbers to guess on, separated by a comma or space")
+
+if " " in ranges: 
+    lower_range, higher_range = ranges.split()
+elif ",":
+    lower_range, higher_range = ranges.split(",")
+else: 
+    print("Enter a valid separator")
+    exit()
+    
+lower_range, higher_range = int(lower_range), int(higher_range)
+machines_number = int(random.randint(lower_range, higher_range))
+chances = 7
+counter = 0
+
+while counter < chances:
+    counter += 1
+    users_number = int(input("Enter a valid number to play the game: "))
+    if users_number == machines_number: 
+        print(f"Your guess {users_number} is the correct number! Congratulations")
+        break
+    elif users_number > machines_number:
+        print(f"Your guessed number {users_number} is too high, try with a lower number")
+    elif users_number < machines_number:
+        print(f"Your guessed number {users_number} is too low, try a higher number")
+    else: 
+        print("Try to guess a real integer number, maybe that works")
+
+    if counter == chances:
+        print(f"Game over, the number was {machines_number}")
